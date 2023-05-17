d062a47baeae77a47ffe76693f6c00c501a4aa1e
ZhaoyangLyu <1155113942@link.cuhk.edu.hk>
2023-05-17 11:08:48 +0800

    remove unused files

--- pointnet2/slurm_commands.sh
+++
@@ -1,5 +0,0 @@
-srun -p content_generation --gres=gpu:8 --ntasks=1 --ntasks-per-node=1 --kill-on-bad-exit=1 --cpus-per-task=128 --job-name
-srun -p content_generation --gres=gpu:4 --ntasks=1 --ntasks-per-node=1 --kill-on-bad-exit=1 --cpus-per-task=128 --job-name
-srun -p content_generation --gres=gpu:2 --ntasks=1 --ntasks-per-node=1 --kill-on-bad-exit=1 --cpus-per-task=64 --job-name
-srun -p content_generation --gres=gpu:1 --ntasks=1 --ntasks-per-node=1 --kill-on-bad-exit=1 --cpus-per-task=32 --job-name
-srun -p content_generation --ntasks=1 --ntasks-per-node=1 --cpus-per-task=64 --job-name
